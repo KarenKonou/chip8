06157ee46caf13c2c7ede89ecc6119c4321ad1b0
Karen Konou <konoukaren@gmail.com>
2020-04-13 11:49:35 +0200

    Add SDL boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
 #include "cpu.h"
+#include "SDL2/SDL.h"
+#include <iostream>
 
 CPU cpu;
+const int window_widght = 640;
+const int window_height = 320;
+SDL_Window* win;
+SDL_Renderer* ren;
 
-int main() {
+auto initialize_graphics() -> int {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  win = SDL_CreateWindow("Hello World!", 100, 100, window_widght, window_height, SDL_WINDOW_SHOWN);
+  if (win == nullptr) {
+    std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    SDL_Quit();
+    return 1;
+  }
+
+  ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (ren == nullptr){
+    SDL_DestroyWindow(win);
+    std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+  SDL_RenderClear(ren);
+  SDL_RenderPresent(ren);
+
+  return 0;
+}
+
+auto main() -> int { 
+  initialize_graphics();
+
+  std::cout << "Renderer pointer is " << ren << std::endl;
+
   cpu.initialize();
+  SDL_Delay(5000);
+  SDL_Quit();
+  return 0;
 }
