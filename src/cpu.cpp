16401d81da29750d61aa018424d66e077167f18a
Karen Konou <konoukaren@gmail.com>
2020-04-13 22:58:30 +0200

    WIP: Start parsing instructions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -26,6 +26,7 @@ CPU::CPU() {
   opcode = 0;
   I = 0;
   sp = 0;
+  draw_flag = false;
   for (int i = 0; i < 4096; i++) {
     memory[i] = 0;
   }
@@ -50,7 +51,9 @@ CPU::CPU() {
 }
 
 auto CPU::loadProgram(const char* path) -> int {
+  std::cout << "Loading program " << path << std::endl;
   FILE* file = fopen(path, "rb");
+
   if (file == nullptr) {
     std::cout << "Couldn't load program." << std::endl;
     return 1;
@@ -61,14 +64,18 @@ auto CPU::loadProgram(const char* path) -> int {
   rewind(file);
 
   char* buffer = (char*)malloc(sizeof(char) * bufferSize);
+
   if (buffer == NULL) {
     std::cout << "Oh fuck this isn't supposed to happen shit" << std::endl;
     return 1;
   }
+
   fread(buffer, 1, bufferSize, file);
+
   for (int i = 0; i < bufferSize; ++i) {
     memory[i + 512] = buffer[i];
   }
+  
   fclose(file);
   free(buffer);
   return 0;
@@ -77,6 +84,111 @@ auto CPU::loadProgram(const char* path) -> int {
 auto CPU::cycle() -> void {
   opcode = memory[pc] << 8 | memory[pc + 1];
 
-  std::cout << std::hex << opcode << std::endl;
-  pc += 2;
+  switch (opcode & 0xF000) {
+  case 0x0000:
+    switch (opcode & 0x000F) {
+    case 0x0000: // 0x00E0: Clears the screen
+      for (int i = 0; i < 64 * 32; i++) {
+        gfx[i] = 0;
+      }
+      draw_flag = true;
+      pc += 2;
+      break;
+
+    case 0x000E: // 0x00EE: Returns from subroutine
+      break;
+    }
+  case 0x1000: // 0x1NNN: Jumps to address NNN
+    pc = opcode & 0x0FFF;
+    break;
+
+  case 0x2000: // 0x2NNN: Calls subroutine at NNN
+    break;
+
+  case 0x3000: // 0x3XNN: Skips next instruction if VX equals NN
+    if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
+      pc += 4;
+    } else {
+      pc += 2;
+    }
+    break;
+
+  case 0x4000: // 0x4XNN: Skips next instruction if VX doesn't equal NN
+    if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
+      pc += 4;
+    } else {
+      pc += 2;
+    }
+    break;
+
+  case 0x5000: // 0x5XY0: Skips next instruction if VX equals VY
+    if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4]) {
+      pc += 4;
+    } else {
+      pc += 2;
+    }
+    break;
+
+  case 0x6000: // 0x6XNN: Sets VX to NN
+    V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
+    pc += 2;
+    break;
+
+  case 0x7000: // 0x7XNN: Adds NN to VX
+    V[(opcode & 0x0F00) >> 8] += (opcode & 0x00FF);
+    pc += 2;
+    break;
+
+  case 0x8000: // lotta shit here
+    switch (opcode & 0x000F) {
+    case 0x0000: // 0x8XY0: Sets VX to the value of VY
+      V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+      pc += 2;
+      break;
+
+    case 0x0001: // 0x8XY1: Sets VX to bitwise OR of VX and VY
+      V[(opcode & 0x0F00) >> 8] =
+          (V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4]);
+      pc += 2;
+      break;
+
+    case 0x0002: // 0x8XY2: Sets VX to bitwise AND of VX and VY
+      V[(opcode & 0x0F00) >> 8] =
+          (V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4]);
+      pc += 2;
+      break;
+
+    case 0x0003: // 0x8XY3: Sets VX to bitwise XOR of VX and VY
+      V[(opcode & 0x0F00) >> 8] =
+          (V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4]);
+      pc += 2;
+      break;
+
+    case 0x0004: // 0x8XY4: Adds VY to VX (sets carry flag)
+      if (V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8]))
+        V[0xF] = 1; // carry
+      else
+        V[0xF] = 0;
+      V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
+      pc += 2;
+      break;
+
+    case 0x0005: // 0x8XY5: Substract VY from FX (VF zero'd when borrowing)
+      if (V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8])
+        V[0xF] = 0; // borrow
+      else
+        V[0xF] = 1;
+      V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
+      pc += 2;
+      break;
+    }
+  case 0xA000: // 0xANNN: Set I to N
+    I = opcode & 0x0FFF;
+    pc += 2;
+    break;
+
+  default:
+    std::cout << "Unrecognised opcode 0x" << std::hex << opcode << std::endl;
+    pc += 2;
+  }
 }
